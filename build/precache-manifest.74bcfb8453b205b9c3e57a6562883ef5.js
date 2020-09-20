ea71b639db2a7fe61598650adff6f078ebd8a705
bbajpai <b.bhumi101@gmail.com>
2020-09-20 17:19:51 +0530

    a

---
+++ build/precache-manifest.74bcfb8453b205b9c3e57a6562883ef5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "330a966da9171afc971d92d8d6c21657",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f849dbad2611c6f7cdfb",
+    "url": "/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "3055a5c0a2e791114152",
+    "url": "/static/css/main.52047fbe.chunk.css"
+  },
+  {
+    "revision": "f849dbad2611c6f7cdfb",
+    "url": "/static/js/2.a8415090.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a8415090.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3055a5c0a2e791114152",
+    "url": "/static/js/main.7c25ce18.chunk.js"
+  },
+  {
+    "revision": "0529a98c11a206755e39",
+    "url": "/static/js/runtime-main.99490353.js"
+  }
+]);
